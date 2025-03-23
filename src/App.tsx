0ec8aa88e49fb1cce6f81a8d5ff7016dd04ee9ed
junaidbinjaman <me@junaidbinjaman.com>
2025-03-23 10:10:00 +0600

    feat: Created some new pages to improve the user experience

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ export default function App() {
           <Route
             path={passwordResetPageSlug}
             element={<PasswordReset />}
-          />
+          /> 
           <Route element={<Header />}>
             <Route
               path={termsAndConditionsPageSlug}
