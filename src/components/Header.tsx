0fa9729f748c831f1fafd6dfd4ae62fd7bb41d2c
junaidbinjaman <me@junaidbinjaman.com>
2025-03-24 10:10:00 +0600

    feat: Header design is in progress but not yet completed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,22 @@
-import { Outlet } from "react-router";
-
+import Avatar from './Avatar';
+import Button from './Button';
+import Logo from './Logo';
+import PrimaryHeading from './PrimaryHeading';
+import LogoSrc from '/portfolio-card-generator-logo.png';
 
 const Header = () => {
   return (
-    <>
-      <h1>Header</h1>
-      <Outlet />
-    </>
+    <header id='header' className='flex gap-[12.4375rem] items-center justify-center shadow-primary py-5'>
+      <Logo
+        src={LogoSrc}
+        width={167}
+      />
+      <PrimaryHeading classes='text-3xl font-bold text-transparent bg-linear-to-r from-accent1 to-accent2 bg-clip-text leading-relaxed'>Dashboard</PrimaryHeading>
+      <div className='flex gap-[1.875rem] items-center justify-center'>
+        <Button classes='text-white'>Generate Card</Button>
+        <Avatar />
+      </div>
+    </header>
   );
 };
 
