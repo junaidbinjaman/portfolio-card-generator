ee27fe063230a751003fcde1755e1418fddc5a75
junaidbinjaman <me@junaidbinjaman.com>
2025-03-27 13:11:53 +0600

    feat: Setted up project to start development. - Inatalled react using vite - INstalled tailwindcss for vite to design the ui - Writted README.md file long with license.txt file - Added .gitignore file - Removed all the vite's bilerplate code Note: Unit testing libraries are not yet installed. Installing them will make this project ready to go.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Logo from './assets/portfolio-card-generator0log.png'
+
+function App() {
+
+
+  return (
+    <>
+    <img src={Logo} alt="" />
+    <h1 className='text-2xl'>Hello World..</h1>
+    </>
+  )
+}
+
+export default App
