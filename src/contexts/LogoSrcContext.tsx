0231afbe039f245316e270182262d0342eb74ce3
junaidbinjaman <me@junaidbinjaman.com>
2025-04-30 06:20:40 +0600

    The logo src is now fully cached and memory leaks are handled nicely. So expecting no more momory leaks here in this case

--- src/contexts/LogoSrcContext.tsx
+++ src/contexts/LogoSrcContext.tsx
@@ -1,4 +1,4 @@
-import React, {memo} from 'react';
+import React, { memo, useRef } from 'react';
 import { createContext, useContext, useEffect, useState } from 'react';
 
 interface LogoSrcContextProviderProps {
@@ -15,37 +15,50 @@ const LogoSrcContext = createContext<APIResponse | null>(null);
 
 const LogoSrcContextProvider = memo(({ children }: LogoSrcContextProviderProps) => {
   const [logoSrc, setLogoSrc] = useState<APIResponse | null>(DEFAULT_LOGO);
+  const cache = useRef<APIResponse | null>(null);
 
   useEffect(() => {
     const controller = new AbortController();
-    const timeoutId = setTimeout(() => controller.abort(), 5000); // 5-second timeout
+    const signal = controller.signal;
+
+    if (cache.current) {
+      setLogoSrc(cache.current);
+      return;
+    }
+
+    const timeOutId = setTimeout(() => {
+      controller.abort();
+    }, 5000);
 
     (async () => {
       try {
-        const response = await fetch(`${import.meta.env.VITE_API_URL}/logo`, { signal: controller.signal });
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/logo`, {
+          signal,
+        });
+
         if (!response.ok) {
           throw new Error(`Failed to fetch logo: ${response.statusText}`);
         }
+
         const data: APIResponse = await response.json();
 
         if (data.logoSrc && data.alt) {
+          cache.current = data;
           setLogoSrc(data);
         } else {
           console.warn('Invalid API response structure:', data);
         }
       } catch (error) {
-        // @ts-expect-error expect name error
-        if (error.name === 'AbortError') {
-          console.error('Fetch request timed out');
-        } else {
-          console.error('Error fetching logo:', error);
-        }
+        console.error('Error fetching logo:', error);
       } finally {
-        clearTimeout(timeoutId);
+        clearTimeout(timeOutId);
       }
     })();
 
-    return () => controller.abort();
+    return () => {
+      clearTimeout(timeOutId);
+      controller.abort();
+    };
   }, []);
 
   return <LogoSrcContext.Provider value={logoSrc}>{children}</LogoSrcContext.Provider>;
